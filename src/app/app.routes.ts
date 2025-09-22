f08495c19d9c916aa1b8d573a9b14c6d279fe16c
SIRAJ-ANSARI <BOOOOM86608@gmail.com>
2025-09-22 18:17:11 +0500

    Added routes and additional components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { AboutUs } from './about-us/about-us';
+import { Podcast } from './podcast/podcast';
+import { Jobs } from './jobs/jobs';
+import { Services } from './services/services';
+import { Gallery } from './gallery/gallery';
+import { ContactUs } from './contact-us/contact-us';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'about-us',
+    component: AboutUs,
+  },
+  {
+    path: 'podcast',
+    component: Podcast,
+  },
+  {
+    path: 'jobs',
+    component: Jobs,
+  },
+  {
+    path: 'services',
+    component: Services,
+  },
+  {
+    path: 'gallery',
+    component: Gallery,
+  },
+  {
+    path: 'contact-us',
+    component: ContactUs,
+  },
+];
