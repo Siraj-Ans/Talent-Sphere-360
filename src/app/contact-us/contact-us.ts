e150ad4aee6d10d29c5993c676ff9854b5f161be
SIRAJ-ANSARI <BOOOOM86608@gmail.com>
2025-09-24 01:05:46 +0500

    Developed contact-us component

--- src/app/contact-us/contact-us.ts
+++ src/app/contact-us/contact-us.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './contact-us.html',
-  styleUrl: './contact-us.css'
+  styleUrl: './contact-us.css',
 })
 export class ContactUs {
+  firstName!: string;
+  lastName!: string;
+  city!: string;
+  country!: string;
+  phoneNo!: string;
+  email!: string;
+  organization!: string;
+  designation!: string;
+  noOfEmployees!: number;
+  message?: string;
 
+  onClick() {
+    const formData = new FormData();
+  }
 }
