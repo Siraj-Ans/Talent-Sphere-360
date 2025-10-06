3d845ba0518048f59f57b06451cd4a7251f1e880
Siraj Ansari <boooom86608@gmail.com>
2025-10-07 02:49:31 +0500

    Added carousel and made some slight design chnages to about-us page.

--- src/app/about-us/about-us.ts
+++ src/app/about-us/about-us.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Footer } from '../shared/footer/footer';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -13,7 +13,43 @@ gsap.registerPlugin(ScrollTrigger);
   styleUrl: './about-us.css',
 })
 export class AboutUs implements AfterViewInit {
+  @ViewChild('carouselWrapper') carouselWrapper!: ElementRef;
+  currentIndex = 0;
+  itemWidth = 0;
+  visibleItems = 0;
+  clientComments = [
+    {
+      link: 'https://www.instagram.com/ramenstation_rwp?igsh=MWQ0Njg4ZTYwMTlsZg==',
+      image: 'assets/Icons/productivity-ability-talent-productive-svgrepo-com.svg',
+      name: 'XILIKE[XIAMEN] CONSULTANCY PVT.LTD. - CHINA',
+      comment:
+        'The Sphere Partnership Plans look very flexible. Having different options for startups, MEs, and corporates makes it much easier to choose the right level of support.',
+    },
+    {
+      link: 'https://www.instagram.com/deaestheticlounge?igsh=MTB0cjN0dmkxc3U4bw==',
+      image: 'assets/Images/deaestheticlounge.jpg',
+      name: 'De Aesthetics',
+      comment:
+        'I like how your TALENT values are not just words but seem built into your approach. Transparency and empathy really stand out to me as unique.',
+    },
+    {
+      link: '',
+      image: 'assets/Images/Rammen Station.jpg',
+      name: 'Rammen Station - Rawalpindi',
+      comment:
+        'Really impressed with the clarity of your services! The way you’ve aligned HR with purpose-driven growth is exactly what many businesses need today.',
+    },
+    {
+      link: 'https://www.linkedin.com/company/redonfilms/',
+      image: 'assets/Images/Rammen Station.jpg',
+      name: 'Rednon Films',
+      comment:
+        'Really impressed with the clarity of your services! The way you’ve aligned HR with purpose-driven growth is exactly what many businesses need today.',
+    },
+  ];
+
   ngAfterViewInit(): void {
+    this.calculateSizes();
     const hero = document.querySelector('.hero-section') as HTMLElement;
 
     if (!hero) return;
@@ -26,5 +62,40 @@ export class AboutUs implements AfterViewInit {
       pin: true,
       pinSpacing: false, // avoids extra white space
     });
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.calculateSizes();
+  }
+
+  calculateSizes() {
+    const wrapper = this.carouselWrapper.nativeElement as HTMLElement;
+    const item = wrapper.querySelector('.carousel-item') as HTMLElement;
+    if (!item) return;
+
+    // Get item width including the gap
+    this.itemWidth = item.offsetWidth + 16; // 16px = Tailwind gap-4
+    const containerWidth = wrapper.offsetWidth;
+
+    // Calculate how many items can fit exactly
+    this.visibleItems = containerWidth / this.itemWidth;
+  }
+
+  next() {
+    const maxIndex = Math.ceil(this.clientComments.length - this.visibleItems);
+
+    if (this.currentIndex < maxIndex) {
+      this.currentIndex++;
+    } else {
+      // Ensure it doesn't go beyond the last fully visible frame
+      this.currentIndex = maxIndex;
+    }
+  }
+
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
   }
 }
