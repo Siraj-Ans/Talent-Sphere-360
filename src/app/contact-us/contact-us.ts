7ffd4c75fdb754fd06933bc4e592a182db0e860b
Siraj Ansari <boooom86608@gmail.com>
2025-10-23 01:15:47 +0500

    Completed Services Component

--- src/app/contact-us/contact-us.ts
+++ src/app/contact-us/contact-us.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
 import { Footer } from '../shared/footer/footer';
 
 @Component({
--- src/app/services/services.ts
+++ src/app/services/services.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 import { Footer } from '../shared/footer/footer';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-services',
-  imports: [Footer],
+  imports: [FormsModule, Footer],
   templateUrl: './services.html',
   styleUrl: './services.css',
 })
-export class Services {}
+export class Services {
+  firstName!: string;
+  lastName!: string;
+  city!: string;
+  country!: string;
+  phoneNo!: string;
+  email!: string;
+  organization!: string;
+  designation!: string;
+  noOfEmployees!: number;
+  message?: string;
+  services = [
+    { name: 'Talent Acquisition & Hiring', plans: ['Basic', 'Premium', 'Enterprise'] },
+    { name: 'Startup HR Support', plans: ['Launch', 'Growth', 'Scale'] },
+    { name: 'Learning & Development (L&D)', plans: ['Workshop', 'Leadership', 'Custom'] },
+    { name: 'Employee Engagement & Wellbeing', plans: ['Survey', 'Wellbeing+', 'Full Suite'] },
+  ];
+
+  plans = ['Sphere Basic', 'Sphere Growth', 'Sphere Elite', 'Custom'];
+  selectedService: string | null = null;
+  selectedPlan: string | null = null;
+
+  onClick() {
+    const formData = new FormData();
+  }
+
+  get availablePlans(): string[] {
+    const service = this.services.find((s) => s.name === this.selectedService);
+    return service ? service.plans : [];
+  }
+
+  onSelectionChange() {
+    console.log('Selected Service:', this.selectedService);
+    console.log('Selected Plan:', this.selectedPlan);
+  }
+}
